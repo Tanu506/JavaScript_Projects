8f423181afcded784333f08e0e20b0b57644fcfc
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-04 23:29:46 +0530

    Adding Js to calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,18 @@
 const input = document.getElementById('display');
+const btns = document.querySelectorAll('button')
+
+btns.forEach(function(btn){
+   btn.addEventListener("click",function(){
+   const val = btn.innerText;
+
+   if(val == "C"){
+     input.value = "";
+   }else if(val == "DEL"){
+     input.value = input.value.slice(0,-1);
+   }else if(val == "="){
+     input.value = eval(input.value);
+   }else{
+     input.value += val;
+   }
+  })
+})
