fabb17c427eca2bd34c97882e9c9d7a4f8244593
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-17 20:35:49 +0530

    Adding JavaScript FIlw which fetches data of weather

--- weather/script.js
+++ weather/script.js
@@ -1,17 +1,44 @@
-fetch(`https://geocoding-api.open-meteo.com/v1/search?name=Gwalior&count=1&language=en&format=json`)
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
-    console.log(data.results[0].id)
-}).catch((error)=>{
-    console.log(error)
-})
+const input = document.querySelector("#cityInput");
+const searchbtn = document.querySelector("#searchBtn");
+const error = document.querySelector("#error");
+const city = document.querySelector("#city");
+const weatherIcon = document.querySelector(".weatherIcon");
+const temperature = document.querySelector("#temperature");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const rain = document.querySelector("#rain");
 
-fetch(`https://api.open-meteo.com/v1/forecast?latitude=26.2183&longitude=78.1828&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m`)
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
-    console.log(data.latitude)
-}).catch((error)=>{
-    console.log(error);
-})
+let latitude;
+let longitude;
+searchbtn.addEventListener("click", function () {
+  city.innerText = input.value;
+  fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${input.value}&count=1&language=en&format=json`,
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      latitude = data.results[0].latitude;
+      longitude = data.results[0].longitude;
+
+      fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m`,
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          humidity.innerText = data.current.relative_humidity_2m;
+          wind.innerText = data.current.wind_speed_10m;
+          rain.innerText = data.current.precipitation;
+          temperature.innerText = `${data.current.temperature_2m}°C`;
+        })
+        .catch((e) => {
+          error.innerText = e;
+        });
+    })
+    .catch((e) => {
+      error.innerText = e;
+    });
+});
