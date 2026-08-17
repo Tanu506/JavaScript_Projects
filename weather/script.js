dfe27332d5529315140aef694a96ea2c94f56874
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-17 23:20:24 +0530

    changing weather icons and correct the latitute and longitude

--- weather/script.js
+++ weather/script.js
@@ -2,14 +2,12 @@ const input = document.querySelector("#cityInput");
 const searchbtn = document.querySelector("#searchBtn");
 const error = document.querySelector("#error");
 const city = document.querySelector("#city");
-const weatherIcon = document.querySelector(".weatherIcon");
+const weatherIcon = document.querySelector("#weatherIcon");
 const temperature = document.querySelector("#temperature");
 const humidity = document.querySelector("#humidity");
 const wind = document.querySelector("#wind");
 const rain = document.querySelector("#rain");
 
-let latitude;
-let longitude;
 searchbtn.addEventListener("click", function () {
   city.innerText = input.value;
   fetch(
@@ -19,8 +17,8 @@ searchbtn.addEventListener("click", function () {
       return response.json();
     })
     .then((data) => {
-      latitude = data.results[0].latitude;
-      longitude = data.results[0].longitude;
+      const latitude = data.results[0].latitude;
+      const longitude = data.results[0].longitude;
 
       fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m`,
@@ -33,6 +31,30 @@ searchbtn.addEventListener("click", function () {
           wind.innerText = data.current.wind_speed_10m;
           rain.innerText = data.current.precipitation;
           temperature.innerText = `${data.current.temperature_2m}°C`;
+          let code = data.current.weather_code;
+          if (code === 0) {
+            weatherIcon.innerText = "☀️";
+          } else if (code === 1) {
+            weatherIcon.innerText = "🌤️";
+          } else if (code === 2) {
+            weatherIcon.innerText = "⛅";
+          } else if (code === 3) {
+            weatherIcon.innerText = "☁️";
+          } else if (code === 45 || code === 48) {
+            weatherIcon.innerText = "🌫️";
+          } else if (code >= 51 && code <= 57) {
+            weatherIcon.innerText = "🌦️";
+          } else if (code >= 61 && code <= 67) {
+            weatherIcon.innerText = "🌧️";
+          } else if (code >= 71 && code <= 77) {
+            weatherIcon.innerText = "❄️";
+          } else if (code >= 80 && code <= 82) {
+            weatherIcon.innerText = "🌦️";
+          } else if (code >= 95) {
+            weatherIcon.innerText = "⛈️";
+          } else {
+            weatherIcon.innerText = "🌡️";
+          }
         })
         .catch((e) => {
           error.innerText = e;
