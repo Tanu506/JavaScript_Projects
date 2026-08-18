0a1157d305d5a46136dcd83c30d0c2f16abc6d1d
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-18 21:33:57 +0530

    Adding Js file (questions funtion showQuestion and nextBtn event listener)

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -1,13 +1,57 @@
 const questionNumber = document.querySelector("#questionNumber");
 const progress = document.querySelector("#progress");
 const question = document.querySelector("#question");
-const option = document.querySelectorAll('.option');
+const options = document.querySelector("#options");
 const nextBtn = document.querySelector("#nextBtn");
-const score = document.querySelector("#score");
+const scoreDislay = document.querySelector("#score");
 const restartBtn = document.querySelector("#restartBtn");
+const result = document.querySelector("#result");
 
-nextBtn.addEventListener("click",()=>{
-    questionNumber.innerHTML = "Question 2 / 10"
-    progress.style.width = "20%";
-    
-})
+const questions = [
+  {
+    question: "Which language is used to style a webpage?",
+    options: ["HTML", "CSS", "JavaScript", "Python"],
+    answer: "CSS",
+  },
+
+  {
+    question: "Which language is used to make a webpage interactive?",
+    options: ["HTML", "CSS", "JavaScript", "SQL"],
+    answer: "JavaScript",
+  },
+
+  {
+    question: "Which tag is used to create a paragraph in HTML?",
+    options: ["h1", "p", "div", "span"],
+    answer: "p",
+  },
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+function showQuestion() {
+  const current = questions[currentQuestion];
+  questionNumber.innerText = `Question ${currentQuestion + 1} / ${questions.length}`;
+  progress.style.width = `${((currentQuestion + 1) * 100) / questions.length}%`;
+  question.innerText = current.question;
+  options.innerHTML = `<button class="option">${current.options[0]}</button>
+                       <button class="option">${current.options[1]}</button>
+                       <button class="option">${current.options[2]}</button>
+                       <button class="option">${current.options[3]}</button>`;
+  //  options.innerHTML = "";
+  //  current.options.forEach(function(option){
+  //     const button = document.createElement('button');
+  //     button.classList.add("option");
+  //     button.innerText = option;
+  //     options.appendChild(button);
+  //  })
+}
+
+showQuestion();
+nextBtn.addEventListener("click", function () {
+  currentQuestion++;
+  showQuestion();
+});
+
+
