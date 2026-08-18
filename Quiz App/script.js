b9fd5769963020815b95e961f5fc7a59330f3f49
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-18 23:46:31 +0530

    Adding all features including next,start button, score updation

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -7,6 +7,7 @@ const scoreDislay = document.querySelector("#score");
 const restartBtn = document.querySelector("#restartBtn");
 const result = document.querySelector("#result");
 
+
 const questions = [
   {
     question: "Which language is used to style a webpage?",
@@ -35,23 +36,66 @@ function showQuestion() {
   questionNumber.innerText = `Question ${currentQuestion + 1} / ${questions.length}`;
   progress.style.width = `${((currentQuestion + 1) * 100) / questions.length}%`;
   question.innerText = current.question;
-//   options.innerHTML = `<button class="option">${current.options[0]}</button>
-//                        <button class="option">${current.options[1]}</button>
-//                        <button class="option">${current.options[2]}</button>
-//                        <button class="option">${current.options[3]}</button>`;
-   options.innerHTML = "";
-   current.options.forEach(function(option){
-      const button = document.createElement('button');
-      button.classList.add("option");
-      button.innerText = option;
-      options.appendChild(button);
-   })
+  //   options.innerHTML = `<button class="option">${current.options[0]}</button>
+  //                        <button class="option">${current.options[1]}</button>
+  //                        <button class="option">${current.options[2]}</button>
+  //                        <button class="option">${current.options[3]}</button>`;
+  options.innerHTML = "";
+  current.options.forEach(function (option) {
+    const button = document.createElement("button");
+    button.classList.add("option");
+    button.innerText = option;
+    options.appendChild(button);
+  });
 }
 
 showQuestion();
+
+options.addEventListener("click", function (e) {
+  const current = questions[currentQuestion];
+
+  if (e.target.classList.contains("option")) {
+    const allOptions = options.querySelectorAll(".option");
+    allOptions.forEach(function (option) {
+      option.classList.remove("selected");
+    });
+    e.target.classList.add("selected");
+  }
+});
+
 nextBtn.addEventListener("click", function () {
+  const selectedOption = options.querySelector(".selected");
+  if (!selectedOption) {
+    alert("please select an answer!");
+    return;
+  }
+    const current = questions[currentQuestion];
+
+    if (selectedOption.innerText == current.answer) {
+      score++;
+      console.log(score);
+    }
+
+    if(currentQuestion === questions.length - 1){
+        showResult();
+        return
+    }
   currentQuestion++;
   showQuestion();
 });
 
+function showResult(){
+    document.querySelector(".quiz-box").style.display = "none";
+    result.style.display = "block";
+    scoreDislay.innerText = `${score}/${questions.length}`
+    document.querySelector(".quiz-header").style.display = "none";
+}
 
+restartBtn.addEventListener('click',function(){
+    result.style.display = "none";
+    document.querySelector(".quiz-box").style.display = "block";
+    score = 0;
+    currentQuestion = 0;
+    document.querySelector(".quiz-header").style.display = "block";
+    showQuestion();
+})
