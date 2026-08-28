9af7a4aaaf7c9cb1eaf647fa5037218c3c6602b4
Tanu Goyal <tanyagoyal258@gmail.com>
2026-08-28 23:57:36 +0530

    Adding js to expense tracker

---
+++ Expense Tracker/script.js
@@ -0,0 +1,14 @@
+const balance = document.querySelector('#balance');
+const income = document.querySelector('#income');
+const expense = document.querySelector('#expense');
+const form = document.getElementById('transactionForm')
+const transactionInput = document.getElementById('title');
+const amountInput = document.getElementById('amount');
+const transactionType = document.getElementById('type');
+const addTransactionButton = document.querySelector('.add-btn')
+const transactionList = document.getElementById('transactionList')
+
+
+form.addEventListener('submit',function(){
+
+})
