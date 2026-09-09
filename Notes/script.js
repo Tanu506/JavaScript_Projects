ebaefb342d28eaad9fabaaabec19ff86cdaab2f7
Tanu Goyal <tanyagoyal258@gmail.com>
2026-09-09 23:31:18 +0530

    Add js file to notes app where we can add, display and delete notes.

---
+++ Notes/script.js
@@ -0,0 +1,49 @@
+const noteTitle = document.querySelector("#noteTitle");
+const noteContent = document.querySelector("#noteContent");
+const addNoteBtn = document.querySelector("#addNoteBtn");
+const notesContainer = document.querySelector("#notesContainer");
+
+let notes = [];
+
+function displayNote() {
+  notesContainer.innerHTML = "";
+
+  notes.forEach((n) => {
+    notesContainer.innerHTML += `
+        <div class="note">
+           <h3>${n.title}</h3>
+           <p>${n.content}</p>
+           <button class="delete-btn" data-id="${n.id}">Delete</button>
+        </div>
+    `;
+  });
+}
+
+function addNote() {
+  const noteTitleInput = noteTitle.value;
+  const noteContentInput = noteContent.value;
+
+  const noteArray = {
+    id: Date.now(),
+    title: noteTitleInput,
+    content: noteContentInput,
+  };
+
+  notes.push(noteArray);
+  noteTitle.value = "";
+  noteContent.value = "";
+  displayNote();
+}
+
+addNoteBtn.addEventListener("click", addNote);
+
+notesContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-btn")) {
+    const id = Number(event.target.dataset.id);
+
+    notes = notes.filter((n) => {
+      return n.id != id;
+    });
+    displayNote();
+  }
+});
